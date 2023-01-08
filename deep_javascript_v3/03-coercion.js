27319f9cd0a797f80f5a69aaa2e294506290c6de
Michael Shuff <mjshuff23@gmail.com>
2023-01-08 00:56:02 -0500

    starting coercion, nmext video is on toString()

---
+++ deep_javascript_v3/03-coercion.js
@@ -0,0 +1,4 @@
+// Abstract Operations
+// 7.1.1 ToPrimitive ( input [ , PreferredType ] )
+// The abstract operation ToPrimitive takes an input argument and an optional argument PreferredType. The abstract operation ToPrimitive converts its input argument to a non-Object type. This abstract operation functions as follows:
+
